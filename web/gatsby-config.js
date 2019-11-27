860a47908282fe0c8b19f40444ebbb001ad1497a
Alexandre Di Guida <alex@gearedapp.co.uk>
2019-11-27 16:10:27 +0000

    🔧 Gatsby config - enable watch mode for prod

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,6 +1,6 @@
 // Load variables from `.env` as soon as possible
 require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV || 'development'}`
+  path: `.env.${process.env.NODE_ENV || 'development'}`,
 })
 
 const clientConfig = require('./client-config')
@@ -16,9 +16,9 @@ module.exports = {
       options: {
         ...clientConfig.sanity,
         token: process.env.SANITY_READ_TOKEN,
-        watchMode: !isProd,
-        overlayDrafts: !isProd
-      }
-    }
-  ]
+        watchMode: true,
+        overlayDrafts: true,
+      },
+    },
+  ],
 }
